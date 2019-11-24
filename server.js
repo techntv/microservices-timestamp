2059451b59e214abdbd1b1e875ec23b43c5ccf8e
The Vinh <vinh.nguyenthe@haravan.com>
2019-11-24 20:58:25 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,69 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+function isValidTimestamp(time) {
+  let regex = /\d{4}(\-\d{2}){2}/g;
+  if (regex.test(time)) {
+    return {
+      isValid: true,
+      value: time
+    }
+  } else if ((!isNaN(parseFloat(time)) && isFinite(time))) {
+    return {
+      isValid: true,
+      value: parseFloat(time)
+    }
+  } else {
+    return {
+      isValid: false,
+      value: null
+    };
+  }
+
+}
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/timestamp/:date_string", function (req, res) {
+  let timestamp = req.params.date_string
+  if (!timestamp) {
+    timestamp = new Date()
+  }
+
+  let isValidTime = isValidTimestamp(timestamp);
+  if (isValidTime.isValid) {
+    res.json({
+      "unix": new Date(isValidTime.value).getTime(),
+      "utc": new Date(isValidTime.value).toUTCString()
+    });
+  } else {
+    res.json({
+      "unix": null,
+      "utc": "Invalid"
+    });
+  }
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
